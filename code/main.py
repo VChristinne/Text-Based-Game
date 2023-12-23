20150cd7126bf1b2e924acab559bd3fb49614237
Christinne <vanessa0christinne@gmail.com>
2023-12-23 18:37:43 +0100

    Test Combat

--- code/main.py
+++ code/main.py
@@ -1,9 +1,34 @@
+from colorama import Fore, Style
 import life_attributes as life
+import combat_attributes as combat
 
 
 def main():
-    atb01 = life.LifeAttributes(100, 100, 100, 100, 100, 100, 100, 100)
-    status(atb01)
+    player = life.LifeAttributes(100, 100, 100, 100, 100, 100, 100, 100)
+    print(Fore.BLUE + "\n### PLAYER ###")
+    status(player)
+
+    enemy = life.LifeAttributes(100, 100, 100, 100, 100, 100, 100, 100)
+    print(Fore.RED + "\n### ENEMY ###")
+    status(enemy)
+
+    ### Test Life Attributes ###
+    rounds = 0
+    while enemy.get_health() > 0:
+        rounds += 1
+        print(Fore.BLACK + f"\n\n***ROUND {rounds}***" + Fore.RESET)
+
+        ### Test Combat ###
+        player_attack = combat.CombatAttributes(50)
+
+        player_attack.attack(enemy)
+        player.use_stamina(20)
+
+        print(Fore.RED + f"Enemy health: {enemy.get_health()}" + Fore.RESET)
+        print(Fore.RED + f"Enemy shield: {enemy.get_shield()}" + Fore.RESET)
+        print(Fore.BLUE + f"Player stamina: {player.get_stamina()}" + Fore.RESET)
+
+    print(Style.BRIGHT + Fore.GREEN + "\nEnemy defeated!\n" + Style.RESET_ALL)
 
 
 def status(atb):
