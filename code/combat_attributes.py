ee15dcdeca5eb2ab0dbfbcadfee25596710a5d2a
Christinne <vanessa0christinne@gmail.com>
2023-12-23 21:07:10 +0100

    Create def stamina_cost

--- code/combat_attributes.py
+++ code/combat_attributes.py
@@ -1,6 +1,18 @@
 class CombatAttributes:
-    def __init__(self, damage):
+    def __init__(self, damage=50):
         self.damage = damage
+
+    def stamina_cost(self):
+        if 10 < self.damage <= 20:
+            return 5
+        elif 20 < self.damage <= 30:
+            return 8
+        elif 30 < self.damage <= 40:
+            return 10
+        elif 40 < self.damage <= 50:
+            return 12
+        else:
+            return 0
 
     ### Attack ###
     def attack(self, target):
