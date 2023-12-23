b5b795ab314715225642537980b399b80f8f1599
Christinne <vanessa0christinne@gmail.com>
2023-12-23 16:26:14 +0100

    Add Defense attribute

--- code/combat_attributes.py
+++ code/combat_attributes.py
@@ -2,12 +2,20 @@ import life_attributes as life
 
 
 class CombatAttributes(life):
-    def __init__(self, health, max_health, shield, max_shield, stamina, max_stamina, mana, max_mana, damage):
+    def __init__(self, health, max_health, shield, max_shield, stamina, max_stamina, mana, max_mana, damage, defense):
         super().__init__(self, health, max_health, shield, max_shield, stamina, max_stamina, mana, max_mana)
         self.damage = damage
 
+    ### Attack ###
     def attack(self, target):
         target.take_damage(self.damage)
 
     def get_damage(self):
         return self.damage
+
+    ### Defense ###
+    def defense(self, target):
+        target.take_damage(self.defense)
+
+    def get_defense(self):
+        return self.defense
