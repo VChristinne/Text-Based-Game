cf420fab9e289939fa80c3ce7ef316d1e61a0740
Christinne <vanessa0christinne@gmail.com>
2023-12-23 22:30:42 +0100

    Fix stamina cost

--- code/combat_attributes.py
+++ code/combat_attributes.py
@@ -6,11 +6,13 @@ class CombatAttributes:
         if 10 <= self.damage <= 20:
             return 5
         elif 20 < self.damage <= 30:
-            return 8
-        elif 30 < self.damage <= 40:
             return 10
+        elif 30 < self.damage <= 40:
+            return 15
         elif 40 < self.damage <= 50:
-            return 12
+            return 25
+        elif 50 < self.damage <= 60:
+            return 30
         else:
             return 0
 
