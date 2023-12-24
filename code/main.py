32884b32557a4bd33280d25a0b18d75b417a3fce
Christinne <vanessa0christinne@gmail.com>
2023-12-24 01:26:10 +0100

    Code Refactor

--- code/main.py
+++ code/main.py
@@ -6,43 +6,33 @@ import combat_attributes as combat
 
 def main():
     player = life.LifeAttributes()
-    print(Fore.BLUE + "\n### PLAYER ###")
-    status(player)
-
     enemy = life.LifeAttributes()
-    print(Fore.RED + "\n### ENEMY ###")
-    status(enemy)
 
-    player_damage_value = player_dice_roll()
-    basic_combat(player, enemy, player_damage_value)
+    while not player.is_exhausted() and not enemy.is_dead():
+        player_damage_value = player_dice_roll()
+        basic_combat(player, enemy, player_damage_value)
 
 
 def player_dice_roll():
-    """
-    Player can choose between 5 dice rolls (10 to 20, 20 to 30, 30 to 40, 40 to 50, 50 to 60)
-    First dice roll costs 5 stamina, second costs 10, third costs 15, fourth costs 25, fifth costs 30
-    """
-    print(Style.BRIGHT + Fore.MAGENTA + "\nRoll dice (10 to 20, 20 to 30, 30 to 40, 40 to 50, 50 to 60)\n" + Style.RESET_ALL)
-    print(Fore.MAGENTA + "(1) - 10 to 20: 5 stamina\n" + Fore.RESET)
-    print(Fore.MAGENTA + "(2) - 20 to 30: 10 stamina\n" + Fore.RESET)
-    print(Fore.MAGENTA + "(3) - 30 to 40: 15 stamina\n" + Fore.RESET)
-    print(Fore.MAGENTA + "(4) - 40 to 50: 25 stamina\n" + Fore.RESET)
-    print(Fore.MAGENTA + "(5) - 50 to 60: 30 stamina\n" + Fore.RESET)
-    player_dice = input("Choose dice: ")
+    dice_options = {
+        "1": (10, 20, 5),
+        "2": (20, 30, 10),
+        "3": (30, 40, 15),
+        "4": (40, 50, 25),
+        "5": (50, 60, 30)
+    }
 
-    if player_dice == "1":
-        player_damage_value = random.randrange(10, 20, 5)
-    elif player_dice == "2":
-        player_damage_value = random.randrange(20, 30, 5)
-    elif player_dice == "3":
-        player_damage_value = random.randrange(30, 40, 5)
-    elif player_dice == "4":
-        player_damage_value = random.randrange(40, 50, 5)
-    elif player_dice == "5":
-        player_damage_value = random.randrange(50, 60, 5)
-    elif "1" < player_dice < "5":
+    print(Style.BRIGHT + Fore.GREEN + "\nRoll dices\n" + Style.RESET_ALL)
+    print(Fore.GREEN + "\n".join(f"({key}) -> {low} to {high}: {stamina} stamina" for key, (low, high, stamina) in dice_options.items()) + Fore.RESET)
+    player_dice = input("\nChoose dice: ")
+
+    if player_dice in dice_options:
+        low, high, stamina = dice_options[player_dice]
+        player_damage_value = random.randrange(low, high, 2)
+    else:
         print("Invalid input!")
-        player_dice_roll()
+        player_damage_value = player_dice_roll()
+
     return player_damage_value
 
 
@@ -51,11 +41,11 @@ def basic_combat(player, enemy, player_damage_value):
     Player can only roll dice if they have enough stamina
     Player can only roll dice 20 times
     """
-    for rounds in range(0, 20):
+    rounds = 0
+    while not player.is_exhausted() and not enemy.is_dead() and rounds < 20:
         rounds += 1
         print(Style.BRIGHT + Fore.CYAN + f"\n\n***ROUND {rounds}***" + Style.RESET_ALL)
 
-        ### Test Combat ###
         player_attack = combat.CombatAttributes(player_damage_value)
         player_attack.attack(enemy)
         player.use_stamina(player_attack.stamina_cost())
@@ -73,36 +63,7 @@ def basic_combat(player, enemy, player_damage_value):
             print(Style.BRIGHT + Fore.GREEN + "\nEnemy defeated!\n" + Style.RESET_ALL)
             break
 
-    '''
-    rounds = 0
-    while enemy.get_health() > 0:
-        rounds += 1
-        print(Style.BRIGHT + Fore.CYAN + f"\n\n***ROUND {rounds}***" + Style.RESET_ALL)
-
-        ### Test Combat ###
-        player_attack = combat.CombatAttributes(random.randrange(10, 60, 5))
-        player_attack.attack(enemy)
-        player.use_stamina(player_attack.stamina_cost())
-
-        print(Fore.BLUE + f"Player damage: {player_attack.get_damage()}" + Fore.RESET)
-        print(Fore.RED + f"Enemy health: {enemy.get_health()}" + Fore.RESET)
-        print(Fore.RED + f"Enemy shield: {enemy.get_shield()}" + Fore.RESET)
-        print(Fore.BLUE + f"Player stamina: {player.get_stamina()}" + Fore.RESET)
-
-        if player.is_exhausted():
-            print(Style.BRIGHT + Fore.BLUE + "\nPlayer is exhausted!\n" + Style.RESET_ALL)
-            break
-
-        if enemy.is_dead():
-            print(Style.BRIGHT + Fore.GREEN + "\nEnemy defeated!\n" + Style.RESET_ALL)
-            break
-    '''
-
-def status(atb):
-    print("Health: " + str(atb.get_health()) + "/" + str(atb.get_max_health()))
-    print("Shield: " + str(atb.get_shield()) + "/" + str(atb.get_max_shield()))
-    print("Stamina: " + str(atb.get_stamina()) + "/" + str(atb.get_max_stamina()))
-    print("Mana: " + str(atb.get_mana()) + "/" + str(atb.get_max_mana()))
+        player_dice_roll()
 
 
 if __name__ == '__main__':
