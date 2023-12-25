634702da526754e8fc74100331619823b9969006
Christinne <vanessa0christinne@gmail.com>
2023-12-25 16:49:55 +0100

    Update archer_role.py

--- code/archer_role.py
+++ code/archer_role.py
@@ -11,8 +11,7 @@ class Archer(LifeAttributes):
     def __init__(self, health=40, max_health=40, shield=20, max_shield=20, stamina=30, max_stamina=30, mana=40, max_mana=40):
         super().__init__(health, max_health, shield, max_shield, stamina, max_stamina, mana, max_mana)
         self.abilities = ["Piercing Shot", "Arcane Arrow", "Arrow of Silence", "Solar Flare Shot"]
-        self.ability_damage = [0, 30, 0, 50]
-        self.ability_stamina_cost = [20, 10, 30, 40]
-        self.ability_mana_cost = [0, 0, 20, 0]
+        self.ability_damage = [0, 0, 0, 0]
+        self.ability_stamina_cost = [0, 0, 0, 0]
+        self.ability_mana_cost = [0, 0, 0, 0]
         self.current_ability = None
-    
