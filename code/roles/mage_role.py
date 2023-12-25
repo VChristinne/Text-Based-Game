c35088f770f664fcd052403ee34cb9355c152e7f
Christinne <vanessa0christinne@gmail.com>
2023-12-25 12:49:06 +0100

    Create mage_role.py

---
+++ code/roles/mage_role.py
@@ -0,0 +1,16 @@
+import life_attributes as life
+
+
+class Mage(life.LifeAttributes):
+    """
+    1. Soulbind: binds the soul of an enemy.
+    2. Dante's Inferno: deals massive fire damage to all enemies.
+    3. Quantum Surge: deals variable quantum damage to an enemy.
+    4. Void Shatter: instantly kills an enemy without shield with 25 health.
+    """
+    def __init__(self, health=60, max_health=60, shield=0, max_shield=0, stamina=0, max_stamina=0, mana=80, max_mana=80):
+        super().__init__(health, max_health, shield, max_shield, stamina, max_stamina, mana, max_mana)
+        self.abilities = ["Soulbind", "Dante's Inferno", "Quantum Surge", "Void Shatter"]
+        self.ability_damage = [0, 50, 20, 100]
+        self.ability_stamina_cost = [30, 40, 10, 70]
+        self.current_ability = None
