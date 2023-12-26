d2f525ffb077adf0345b8b598d1430faa7d8b5bc
Christinne <vanessa0christinne@gmail.com>
2023-12-26 17:08:36 +0100

    Add class for each role skill

--- code/skill_roles.py
+++ code/skill_roles.py
@@ -5,3 +5,22 @@ class ArcherSkill(Card):
     def __init__(self, name, description, stamina_cost, mana_cost, damage, critic):
         super().__init__(name, description, damage, stamina_cost, mana_cost)
         self.critic = critic
+
+
+class AssassinSkill(Card):
+    def __init__(self, name, description, stamina_cost, mana_cost, damage, critic, speed):
+        super().__init__(name, description, damage, stamina_cost, mana_cost)
+        self.critic = critic
+        self.speed = speed
+
+
+class KnightSkill(Card):
+    def __init__(self, name, description, stamina_cost, mana_cost, damage, defense):
+        super().__init__(name, description, damage, stamina_cost, mana_cost)
+        self.defense = defense
+
+
+class MageSkill(Card):
+    def __init__(self, name, description, stamina_cost, mana_cost, damage, bind):
+        super().__init__(name, description, damage, stamina_cost, mana_cost)
+        self.bind = bind
