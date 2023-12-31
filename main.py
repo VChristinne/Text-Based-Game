378ce8fb172fa3b8bce77e5b4d55d3903306167f
Christinne <vanessa0christinne@gmail.com>
2023-12-31 15:39:54 +0100

    Skills appear correctly

--- main.py
+++ main.py
@@ -19,29 +19,37 @@ def main():
     if formatted_skills:
         print(Style.BRIGHT + "Skills: " + Style.RESET_ALL)
         print(Style.BRIGHT + '\n'.join(formatted_skills) + Style.RESET_ALL)
-        waves()
-    else:
-        print(Fore.RED + "Classe não reconhecida. Saindo do jogo." + Fore.RESET)
+
+    waves()
 
 
 def waves():
     waves: int = 0
     while enemy_status.get_health() > 0:
         waves += 1
-        print(Fore.BLACK + f"\n\n***WAVE {waves}***" + Fore.RESET)
+        print(Style.BRIGHT + Fore.BLUE + f"\n***WAVE {waves}***" + Style.RESET_ALL)
 
         try:
             player_choice = int(input("Digite o número da habilidade desejada: ")) - 1
-            selected_ability = player_in_game.get_skills()[player_choice]
+            player_in_game.current_skill = player_in_game.get_skills()[player_choice]
 
             if 0 <= player_choice < len(player_in_game.get_skills()):
-                print(f"Você escolheu: {selected_ability}")
+                selected_skill_info = player_in_game.current_skill
+                selected_skill_lines = selected_skill_info.split('\n')
+
+                for index in range(7):
+                    display_skill_info(selected_skill_lines, index)
+
             else:
                 print("Escolha inválida. Tente novamente.")
+
         except IndexError:
             print("Entrada inválida. Digite um número.")
 
     print(Style.BRIGHT + Fore.GREEN + "\nEnemy defeated!\n" + Style.RESET_ALL)
+
+def display_skill_info(skill_lines, index):
+    print(skill_lines[index] if index < len(skill_lines) else "")
 
 
 if __name__ == '__main__':
