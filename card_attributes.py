4b47dbcc22bf6d548c89557e9dc793d70bc1cb24
Christinne <vanessa0christinne@gmail.com>
2024-05-03 12:19:31 +0200

    Delete old code

--- card_attributes.py
+++
@@ -1,7 +0,0 @@
-class Card:
-    def __init__(self, name, description, damage, stamina_cost, mana_cost):
-        self.name = name
-        self.description = description
-        self.damage = damage
-        self.stamina_cost = stamina_cost
-        self.mana_cost = mana_cost
