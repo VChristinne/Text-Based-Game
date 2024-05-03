b4212f7f5315b8b0952822aefc736fcb3a309394
Christinne <vanessa0christinne@gmail.com>
2024-05-03 18:20:49 +0200

    Refactor code

--- main.py
+++ main.py
@@ -1,55 +1,9 @@
-from prettytable import PrettyTable
-from colorama import Fore, Style
-from game_setup import roles
+import game_setup
 
-def role_info(role_name):
-    role = roles[role_name]
-
-    role_table = PrettyTable()
-    role_table.field_names = ["Role", "Health", "Stamina", "Mana", "Shield"]
-    role_table.add_row([role.name, role.health, role.stamina, role.mana, role.shield])
-    print(Fore.RED + Style.BRIGHT + "\nRole Info:" + Fore.RESET)
-    print(role_table)
-
-    print(Fore.RED + Style.BRIGHT + "\nSkills:" + Fore.RESET)
-    for i, skill in enumerate(role.skills, start=1):
-        print(f"{i}. {skill.name}")
-    print(Style.RESET_ALL)
-
-def skill_info(role_name, skill_index):
-    role = roles[role_name]
-    skill = role.get_skill(skill_index - 1)
-
-    if skill:
-        print(Fore.RED + Style.BRIGHT + f"\nSkill {skill_index}:" + Fore.RESET)
-        print(f"{skill.name}\n"
-              f"{skill.description}\n"
-              f"Damage: {skill.damage}\n"
-              f"Health Cost: {skill.health_cost}\n"
-              f"Stamina Cost: {skill.stamina_cost}\n"
-              f"Mana Cost: {skill.mana_cost}")
-        print(Style.RESET_ALL)
-    else:
-        print("Invalid skill index.")
-        menu_role()
-
-def menu_role():
-    print(Fore.RED + Style.BRIGHT + "\nROLES AVAILABLES:" + Fore.RESET)
-    for role_name in roles:
-        print(role_name)
-    print(Style.RESET_ALL)
-
-    role_name = input(Fore.BLUE + Style.BRIGHT + "> Enter role name: " + Style.RESET_ALL).capitalize()
-    if role_name in roles:
-        role_info(role_name)
-        skill_index = int(input(Fore.BLUE + Style.BRIGHT + "> Enter skill index: " + Style.RESET_ALL))
-        skill_info(role_name, skill_index)
-    else:
-        print("Invalid role name.")
-        menu_role()
 
 def main():
-    menu_role()
+    game_setup.menu_role()
+
 
 if __name__ == '__main__':
     main()
