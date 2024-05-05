60723fca4c3b7ee6c9dec44c9e7e50cb27cf65c3
Christinne <vanessa0christinne@gmail.com>
2024-05-05 21:28:39 +0200

    Update test_game.py

--- test_game.py
+++ test_game.py
@@ -1,7 +1,8 @@
+import unittest
 from unittest.mock import patch
 import game_setup
 
-class TestGame:
+class TestGame(unittest.TestCase):
     def test_menu_role(self):
         with patch('builtins.input', side_effect=['Mage', '1']):
             game_setup.menu_role()
@@ -9,3 +10,6 @@ class TestGame:
     def test_role_info(self):
         game_setup.enemies_info('Ghost')
         game_setup.enemy_skill_info('Ghost', 1)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
