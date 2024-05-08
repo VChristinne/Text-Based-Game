10415c87a8ba0e492d70819699f32dd2f7dddc70
Christinne <vanessa0christinne@gmail.com>
2024-05-08 14:33:17 +0200

    Remove function to test_game

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import game_setup
 from element import Element
 
 def main():
-    # game_setup.menu_role()
-    individualism_list = list(Element.individualism_to_color.keys())
-    print(individualism_list)
+    game_setup.menu_role()
 
 if __name__ == '__main__':
     main()
