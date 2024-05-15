a51a1b50d4266bdc8d0b662fa210f7d292f5a7cf
Christinne <vanessa0christinne@gmail.com>
2024-05-15 20:51:28 +0200

    Add main menu

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import game_setup
 
 def main():
-    game_setup.menu_role()
+    # game_setup.menu_role()
+    # game_setup.menu_skill()
+    # game_setup.menu_elements()
+    game_setup.main_menu()
 
 if __name__ == '__main__':
     main()
