65fb766ad74edd4440b63fc906e1f18380a6cc03
Christinne <vanessa0christinne@gmail.com>
2024-06-06 16:40:20 +0200

    Add board func

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import datetime
 import time
-
 import game_board
 import game_setup
 
-def main():
-    username, role_name = game_setup.main_menu()
 
-    players = [game_board.player(username, role_name, "element")]
-    turns = [game_board.turn(1, time.localtime(), time.localtime())]
+def main():
+    username, role_name, element_name = game_setup.main_menu()
+
+    players = [game_board.player(username, role_name, element_name)]
+    turn = game_board.turn(1, datetime.datetime.now(), None)
+    turns = [turn]
     match_obj = game_board.match([players], 1)
 
+    time.sleep(5)
+
+    turn['end_at'] = datetime.datetime.now()
     board = game_board.create_board(match_obj, players, turns)
     game_setup.print_board(board)
 
