510c04645d5a9dee2e62706eab8301de08808402
Christinne <vanessa0christinne@gmail.com>
2024-06-06 12:53:20 +0200

    Testing create_board

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import datetime
+import time
+
+import game_board
 import game_setup
 
 def main():
-    # game_setup.menu_role()
-    # game_setup.menu_skill()
-    # game_setup.menu_elements()
-    game_setup.main_menu()
+    username, role_name = game_setup.main_menu()
+
+    players = [game_board.player(username, role_name, "element")]
+    turns = [game_board.turn(1, time.localtime(), time.localtime())]
+    match_obj = game_board.match([players], 1)
+
+    board = game_board.create_board(match_obj, players, turns)
+    game_setup.print_board(board)
 
 if __name__ == '__main__':
     main()
