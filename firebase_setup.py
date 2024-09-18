5d2ab8ea5f2d056d229d3661b6abb671f726a747
Christinne <vanessa0christinne@gmail.com>
2024-09-18 20:41:49 +0200

    build: use .env

--- firebase_setup.py
+++ firebase_setup.py
@@ -1,8 +1,17 @@
+import json
+import os
 import firebase_admin
 from firebase_admin import credentials
+from dotenv import load_dotenv
 
+
+load_dotenv()
+firebase_credentials_path = os.getenv("FIREBASE_CREDENTIALS_PATH")
 
 def conect_firebase():
-    cred = credentials.Certificate("credentials.json")
+    with open(firebase_credentials_path, "r") as f:
+        firebase_credentials_dict = json.load(f)
+
+    cred = credentials.Certificate(firebase_credentials_dict)
     firebase_admin.initialize_app(cred)
     print("Firebase Connected")
