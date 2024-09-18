f5693452d121671f6b875cb23939e378f45897d6
Christinne <vanessa0christinne@gmail.com>
2024-09-18 14:47:56 +0200

    build: tests for correctly inputs

--- game_test.py
+++ game_test.py
@@ -2,10 +2,43 @@ import unittest
 from unittest.mock import patch
 import game_setup
 
+
 class TestGame(unittest.TestCase):
-    def test_main_menu(self):
-        with patch('builtins.input', side_effect=['1', 'Yuzu', 'mage']):
+    
+    def test_role_selected(self):
+        inputs = ['1', 'Yuzu', 'mage', 'no']
+        with patch('builtins.input', side_effect=inputs):
+            print(f"\nSimulated inputs: {inputs}")
             game_setup.main_menu()
+    
+    
+    def test_role_reselected(self):
+        inputs = ['1', 'Yuzu', 'mage', 'yes', 'Yz', 'assassin', 'no']
+        with patch('builtins.input', side_effect=inputs):
+            print(f"\nSimulated inputs: {inputs}")
+            game_setup.main_menu()
+    
+    
+    def test_role_info(self):
+        inputs = ['2', 'archer']
+        with patch('builtins.input', side_effect=inputs):
+            print(f"\nSimulated inputs: {inputs}")
+            game_setup.main_menu()
+    
+            
+    def test_element_info(self):
+        inputs = ['3', '1']
+        with patch('builtins.input', side_effect=inputs):
+            print(f"\nSimulated inputs: {inputs}")
+            game_setup.main_menu()
+    
+            
+    def test_skill_info(self):
+        inputs = ['4', 'ghost', '1']
+        with patch('builtins.input', side_effect=inputs):
+            print(f"\nSimulated inputs: {inputs}")
+            game_setup.main_menu()
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
