d91d5dca5df60bca6ed25134c397e72bfae2d279
Christinne <vanessa0christinne@gmail.com>
2024-10-14 14:31:19 +0200

    refact: separate background and window

--- gui/interface.py
+++ gui/interface.py
@@ -6,29 +6,29 @@ class Window:
         master.title("CustomTkinter")
         master.resizable(False, False)
         master.attributes('-fullscreen', True)
-        ctk.set_appearance_mode("light")
+        ctk.set_appearance_mode("system")
         ctk.set_default_color_theme("./theme/nord.json")
 
 
-class SideFrame(ctk.CTkFrame):
-    def __init__(self, master, **kwargs):
-        super().__init__(master, **kwargs)
-        self.label = ctk.CTkLabel(self, text="Testing")
+class Background(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.label = ctk.CTkLabel(self, text=None)
         self.label.grid(row=0, column=0, padx=20, pady=20)
-        
+
 
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
 
         self.window = Window(self)
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=4)
-        self.grid_columnconfigure(1, weight=2) 
+        self.grid_rowconfigure(index=0, weight=1)
+        self.grid_columnconfigure(index=0, weight=4)
+        self.grid_columnconfigure(index=1, weight=2) 
 
         for i in range(2):
-            side_frame = SideFrame(self)
-            side_frame.grid(row=0, column=i, sticky="nswe")
+            bg_frame = Background(self)
+            bg_frame.grid(row=0, column=i, sticky="nswe")
 
 
 app = App()
