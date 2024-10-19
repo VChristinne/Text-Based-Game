aca43c76c987e92df79054f1d4bc293544adb8d0
Christinne <vanessa0christinne@gmail.com>
2024-10-19 19:08:23 +0200

    refact: add return and round func

--- game/gboard.py
+++ game/gboard.py
@@ -1,3 +1,18 @@
+def round(turns):
+    round_obj = {
+        "turns": turns
+    }
+    return round_obj
+
+def turn(round_obj, begin_at, end_at=None, total_time=None):
+    turn_obj = {
+        "rounds": round_obj,
+        "begin_at": begin_at,
+        "end_at": end_at,
+        "total_time": total_time
+    }
+    return turn_obj
+
 def match(players, current_turn):
     match_obj = {
         "players": players,
@@ -5,31 +20,22 @@ def match(players, current_turn):
     }
     return match_obj
 
-def turn(round_obj, begin_at, end_at=None, total_time=None):
-    turn_obj = {
-        "round": round_obj,
-        "begin_at": begin_at,
-        "end_at": end_at,
-        "total_time": total_time
-    }
-    return turn_obj
-
-def create_board(match_obj, players, turns):
-    board = {
-        "match": match_obj,
+def create_board(match_obj, players, rounds):
+    board_obj = {
+        "matches": match_obj,
         "players": players,
-        "turns": turns
+        "rounds": rounds
     }
-    return board
+    return board_obj
 
-def get_board(board):
-    return board
+def get_board(board_obj):
+    return board_obj
 
 def update_board(board, match_obj, players, turns):
-    board["match"] = match_obj
+    board["matches"] = match_obj
     board["players"] = players
     board["turns"] = turns
     return board
 
-def delete_board(board):
+def delete_board(board):  # noqa: F841
     del board
